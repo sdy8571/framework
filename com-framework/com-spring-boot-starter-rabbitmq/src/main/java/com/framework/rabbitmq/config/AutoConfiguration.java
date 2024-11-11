c7489c39022abed85688aa9b789d360364613c00
Sdy8571 <sxpdy8571@163.com>
2024-11-11 20:23:49 +0800

    添加mq

---
+++ com-framework/com-spring-boot-starter-rabbitmq/src/main/java/com/framework/rabbitmq/config/AutoConfiguration.java
@@ -0,0 +1,35 @@
+package com.framework.rabbitmq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class AutoConfiguration {
+
+    public AutoConfiguration() {
+        log.info("=== rabbitMQ 消息队列模块加载 ===");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<?> rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(new Jackson2JsonMessageConverter());
+        return factory;
+    }
+
+}
---
+++ com-framework/com-spring-boot-starter-rabbitmq/src/main/java/com/framework/rabbitmq/core/domain/MqObjectMessage.java
@@ -0,0 +1,12 @@
+package com.framework.rabbitmq.core.domain;
+
+import lombok.Data;
+
+/**
+ * @author sdy
+ * @description
+ * @date 2024/11/11
+ */
+@Data
+public class MqObjectMessage {
+}
